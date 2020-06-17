c3c27dc379f67af03bb609ab94cb400d8526c174
paulwoojungkim <wj2kim@gmail.com>
2020-06-17 20:50:33 +0900

    finished before styling

--- src/components/pageFooterComponents/SocialIcons.jsx
+++ src/components/pageFooterComponents/SocialIcons.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import './SocialIcons.scss';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const socialNames = document.querySelectorAll('.social-name');
+//   socialNames.forEach((name) => {
+//     name.addEventListener('mouseover', (e) => {
+//       e.target.
+//     });
+//   });
+// });
 
 const SocialIcons = () => {
   return (
-    <section id="social-icons">
-      {/* <a href=""></a>
-      <a href=""></a>
-      <a href=""></a>
-      <a href=""></a> */}
-    </section>
+    <div id="social-icons" class="social-icons nav-wrapper">
+      <ul>
+        <a
+          title="Instagram"
+          target="_blank"
+          href="https://www.instagram.com/pvvjk/"
+        >
+          <li class="social-name">INSTAGRAM</li>
+        </a>
+        <a title="Github" target="_blank" href="https://github.com/wj2kim">
+          <li class="social-name">GITHUB</li>
+        </a>
+        <a
+          title="Tistory"
+          target="_blank"
+          href="https://dont-stay-hungry.tistory.com"
+        >
+          <li class="social-name">TISTORY</li>
+        </a>
+      </ul>
+    </div>
   );
 };
 
--- src/pages/pageFooter.jsx
+++ src/pages/pageFooter.jsx
@@ -3,19 +3,18 @@ import SocialIcons from '../components/pageFooterComponents/SocialIcons';
 
 const PageFooter = () => {
   return (
-    <div
-      id="page-footer"
-      class="container-fluid"
-      style={{ borderTop: '1px solid #cfcfcf' }}
-    >
-      <div id="copyright-wrapper">
-        <div id="copyright">
-          <i class="material-icons">copyright</i>
-          Paul Woo Jung Kim 2020. All rights reserved.
+    <div id="page-footer" className="container-fluid">
+      <div className="divider"></div>
+      <div className="row">
+        <div id="social-icons-wrapper" className="col s12 m6 push-m6">
+          <SocialIcons />
         </div>
-      </div>
-      <div id="social-icons-wrapper">
-        <SocialIcons />
+        <div id="copyright-wrapper" className="col s12 m6 pull-m6">
+          <div id="copyright">
+            <i className="material-icons">copyright</i>
+            Paul Woo Jung Kim 2020. All rights reserved.
+          </div>
+        </div>
       </div>
     </div>
   );
