205b70576db9728b3915e10beaf05d84b0c165e5
paulwoojungkim <wj2kim@gmail.com>
2020-06-21 18:49:19 +0900

    working on about layout

--- src/components/pageBodyComponents/About.jsx
+++ src/components/pageBodyComponents/About.jsx
@@ -5,26 +5,55 @@ const About = () => {
   return (
     <section id="content-about" class="container-fluid">
       <div id="content-about-title">
-        <h4>About</h4>
+        <a className="link-2">ABOUT</a>
       </div>
-      <div class="image-stack">
-        <div id="content-about-body" class="col s12 m8 offset-m2 l6 offset-l3">
-          <div class="card-panel white lighten-5 z-depth-1">
-            <div class="row valign-wrapper">
-              <div class="col s2">
-                <img
-                  src="images/mypic.jpg"
-                  alt="paulwoojungkim_selfie"
-                  class="circle responsive-img"
-                />
-              </div>
-              <div class="col s10">
-                <span class="black-text">
-                  This is a square image. Add the "circle" class to it to make
-                  it appear circular.
-                </span>
-              </div>
+      {/* <div id="image-stack" class="image-stack">
+        <div id="content-about-body" class="col s12">
+          <div class="row about-me-wrapper">
+            <div class="col s2">
+              <img
+                src="images/mypic.jpg"
+                alt="paulwoojungkim_selfie"
+                class="circle responsive-img z-depth-1"
+              />
             </div>
+            <div class="col s10">
+              <span class="black-text">
+                This is a square image. Add the "circle" class to it to make it
+                appear circular.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      {/* <div class="header">
+          <div class="logo"></div>
+          <label class="night-mode" for="night" onclick="enableNight()">
+            <i class="fa fa-moon-o"></i>
+          </label>
+        </div> */}
+      <div id="image-stack" class="image-stack">
+        <div class="row about-me">
+          <div className="about-content col m6">
+            <div className="profile-location">
+              <i className="material-icons">room</i>
+              Seoul, S. Korea
+            </div>
+            <div className="profile-name">Paul Woo Jung Kim</div>
+            <div className="profile-title">Junior Front-End Developer</div>
+            <div className="profile-summary">
+              Love makaing interactive web, focusing on front-end technologies.
+              I work best as I can. smart user interface and useful interaction.
+              devloping rich skills on web application.
+            </div>
+          </div>
+          <div className="about-content back-image col m6">
+            <img
+              src="images/profile-side-image4.svg"
+              // style={{ height: '50vh', width: '50vw' }}
+              alt="profile-side-image"
+              class="profile-side-image"
+            />
           </div>
         </div>
       </div>
